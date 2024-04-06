2abb830bae229518a82f92fa52c96d235db008ee
Muny Safitri <munysafitri@gmail.com>
2024-04-06 18:15:54 +0700

    jwt secret

--- routes/jawabankognitif-routes.js
+++ routes/jawabankognitif-routes.js
@@ -2,13 +2,13 @@ import express from 'express';
 
 // import {getAllUser,signup,login} from "../controllers/user-controller.js"
 import { getAllKognitif, postKognitif } from '../controllers/jawabankognitif-controller.js';
-import {auth} from '../middleware/auth.js'
+// import {auth} from '../middleware/auth.js'
 
 const router = express.Router();
 
-router.get("/jawabanKognitif",auth, getAllKognitif)
+router.get("/jawabanKognitif", getAllKognitif)
 
-router.post("/jawabanKognitif",auth, postKognitif )
+router.post("/jawabanKognitif", postKognitif )
 // router.post("/login", login )
 
 export default router;
